4695ed319018a9137122457c342c588fb79c2c37
Muhammad H. Al-Aref <muhammad.alaref96@eng-st.cu.edu.eg>
2017-08-31 08:53:06 +0200

    First pre-release!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='CAM2RESTfulAPI',
-	version='1.0-a0',
+	version='v1.0-a0',
 	packages=find_packages(),
 	include_package_data=True,
 	zip_safe=False,
